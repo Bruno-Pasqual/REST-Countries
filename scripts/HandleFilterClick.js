40a16fe93c54c90fadfc6b200cb4725f7aab0471
Bruno Pasqual <brunoh.pasqual0@gmail.com>
2023-04-27 18:51:26 -0300

    continuando 1.0

--- scripts/HandleFilterClick.js
+++ scripts/HandleFilterClick.js
@@ -1,9 +1,26 @@
 const HandleFilterClick = () => {
+  //! Variables ---
   const filterContainer = document.querySelector('.filter_container');
+  const optionsContainer = document.querySelector('.filter_options_container');
+
+  //! Logic ---
   filterContainer.addEventListener('click', () => {
     const arrowIcon = document.querySelector('.arrow_icon');
     arrowIcon.classList.toggle('active');
-    console.log(arrowIcon);
+
+    //! -- Options logic
+    const filterOptions = document.querySelectorAll('.filter_option');
+    console.log(optionsContainer.classList.contains('hidden'));
+    if (optionsContainer.classList.contains('hidden')) {
+      optionsContainer.style.display = 'flex';
+      optionsContainer.classList.remove('hidden');
+    } else {
+      optionsContainer.classList.add('hidden');
+      setTimeout(() => {
+        optionsContainer.style.display = 'none';
+        console.log('escondido');
+      }, 500);
+    }
   });
 };
 
