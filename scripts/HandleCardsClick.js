b8583974a09f8a9cc9b12bb0286a31317d1b0b75
Bruno Pasqual <brunoh.pasqual0@gmail.com>
2023-04-29 17:14:42 -0300

    ajustando modal

--- scripts/HandleCardsClick.js
+++ scripts/HandleCardsClick.js
@@ -21,38 +21,38 @@ const controlModal = (selectedCountry) => {
   const modal = document.createElement('div');
   modal.classList.add('modal');
   modal.innerHTML = `  <button class="close_modal_btn"> <img src="./scripts/back.png" alt="">  Back</button>
-  <div class="flag_container_modal"></div>
-  <div class="details_country_modal">
-    <p class="country_title">Belgium</p>
+        <div class="flag_container_modal"></div>
+        <div class="details_country_modal">
+          <p class="country_title">Belgium</p>
 
-    <!--  -->
-    <div class="details_wrapper">
+          <!--  -->
+          <div class="details_wrapper">
 
-      <!-- ! separador de blocos -->
-      <div class="first_block">
-        <p class="country_info"><span>Native Name: </span> Belgie</p>
-        <p class="country_info"><span>Population: </span> 11,319,511</p>
-        <p class="country_info"><span>Region: </span> Europe</p>
-        <p class="country_info"><span>Sub Region: </span> Western Europe</p>
-        <p class="country_info"><span>Capital: </span> Brussels</p>
-      </div>
+            <!-- ! separador de blocos -->
+            <div class="first_block">
+              <p class="country_info"><span>Native Name: </span> Belgie</p>
+              <p class="country_info"><span>Population: </span> 11,319,511</p>
+              <p class="country_info"><span>Region: </span> Europe</p>
+              <p class="country_info"><span>Sub Region: </span> Western Europe</p>
+              <p class="country_info"><span>Capital: </span> Brussels</p>
+            </div>
 
-      <!-- ! separador de blocos -->
-      <div class="second_block">
-        <p class="country_info"><span>Top Level Domain: </span> .be</p>
-        <p class="country_info"><span>Currencies </span> Euro</p>
-        <p class="country_info"><span>Languages: </span> Dutch, French, German</p>
-      </div>
-    </div>
-    <!--  -->
+            <!-- ! separador de blocos -->
+            <div class="second_block">
+              <p class="country_info"><span>Top Level Domain: </span> .be</p>
+              <p class="country_info"><span>Currencies </span> Euro</p>
+              <p class="country_info"><span>Languages: </span> Dutch, French, German</p>
+            </div>
+          </div>
+          <!--  -->
 
-    <div class="borders_container">
-      <p class="border_title">Border Countries:</p>
-      <div class="borders_wrapper">
-        <p class="border_country">France</p>
-        <p class="border_country">Germany</p>
-        <p class="border_country">Netherlands</p>
-      </div>`;
+          <div class="borders_container">
+            <p class="border_title">Border Countries:</p>
+            <div class="borders_wrapper">
+              <p class="border_country">France</p>
+              <p class="border_country">Germany</p>
+              <p class="border_country">Netherlands</p>
+            </div>`;
   container.appendChild(modal);
   document.body.style.overflow = 'hidden';
 
@@ -60,6 +60,7 @@ const controlModal = (selectedCountry) => {
     if (e.target.classList.contains('showing_modal')) {
       container.classList.remove('showing_modal');
       document.body.style.overflow = 'auto';
+      container.removeChild(modal);
     }
   });
 };
