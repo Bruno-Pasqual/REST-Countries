1b82666e1ddbae8dbb4796fd371b7404cf746ecb
Bruno Pasqual <brunoh.pasqual0@gmail.com>
2023-04-29 15:07:58 -0300

    testando modal

--- scripts/HandleCardsClick.js
+++ scripts/HandleCardsClick.js
@@ -1,3 +1,69 @@
+const controlModal = (selectedCountry) => {
+  //!Destructuring to easier acess to the informations
+  const {
+    capital,
+    population,
+    name,
+    region,
+    subregion,
+    tld,
+    currencies,
+    languages,
+    flags,
+    borders,
+  } = selectedCountry;
+
+  //! -- Colocando o fundo escuro ao mostrar o modal
+  const container = document.querySelector('.container');
+  container.classList.add('showing_modal');
+
+  //!Criando o modal
+  const modal = document.createElement('div');
+  modal.classList.add('modal');
+  modal.innerHTML = `  <button class="close_modal_btn"> <img src="./scripts/back.png" alt="">  Back</button>
+  <div class="flag_container_modal"></div>
+  <div class="details_country_modal">
+    <p class="country_title">Belgium</p>
+
+    <!--  -->
+    <div class="details_wrapper">
+
+      <!-- ! separador de blocos -->
+      <div class="first_block">
+        <p class="country_info"><span>Native Name: </span> Belgie</p>
+        <p class="country_info"><span>Population: </span> 11,319,511</p>
+        <p class="country_info"><span>Region: </span> Europe</p>
+        <p class="country_info"><span>Sub Region: </span> Western Europe</p>
+        <p class="country_info"><span>Capital: </span> Brussels</p>
+      </div>
+
+      <!-- ! separador de blocos -->
+      <div class="second_block">
+        <p class="country_info"><span>Top Level Domain: </span> .be</p>
+        <p class="country_info"><span>Currencies </span> Euro</p>
+        <p class="country_info"><span>Languages: </span> Dutch, French, German</p>
+      </div>
+    </div>
+    <!--  -->
+
+    <div class="borders_container">
+      <p class="border_title">Border Countries:</p>
+      <div class="borders_wrapper">
+        <p class="border_country">France</p>
+        <p class="border_country">Germany</p>
+        <p class="border_country">Netherlands</p>
+      </div>`;
+  container.appendChild(modal);
+  document.body.style.overflow = 'hidden';
+
+  window.addEventListener('click', (e) => {
+    if (e.target.classList.contains('showing_modal')) {
+      container.classList.remove('showing_modal');
+      document.body.style.overflow = 'auto';
+    }
+  });
+};
+
 const HandleCardsClick = () => {
   const cards = document.querySelectorAll('.country_card');
   cards.forEach((card) => {
@@ -14,18 +80,7 @@ const HandleCardsClick = () => {
         return country.name.common === clickedCountryName ? true : false;
       });
 
-      const {
-        capital,
-        population,
-        name,
-        region,
-        subregion,
-        tld,
-        currencies,
-        languages,
-        flags,
-        borders,
-      } = selectedCountry;
+      controlModal(selectedCountry);
     });
   });
 };
