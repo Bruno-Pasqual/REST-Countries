ca2dd3bf0ea55f8c8677e7a567e57a7401e19e09
Bruno Pasqual <brunoh.pasqual0@gmail.com>
2023-05-01 09:29:31 -0300

    test mobile

--- scripts/HandleBorderClick.js
+++ scripts/HandleBorderClick.js
@@ -1,17 +1,23 @@
 import ChangeInfoModal from './ChangeInfoModal.js';
 
 const HandleBorderClick = () => {
+  //!Selecting elements on DOM
   const neighbourCountries = document.querySelectorAll('.border_country');
+  const modalContainer = document.querySelector('.modal');
+
   neighbourCountries.forEach((country) => {
     country.addEventListener('click', (e) => {
       const countries = JSON.parse(sessionStorage.getItem('allCountries'));
       const selectedCountry = countries.filter((country) => {
         return country.name.common === e.target.textContent ? true : false;
       });
+
+      const modalContainer = document.querySelector('.modal');
+      modalContainer.classList.add('changing');
+      console.log(modalContainer);
       ChangeInfoModal(selectedCountry);
 
       //Close modal --
-      const modalContainer = document.querySelector('.modal');
       const closeModal = document.querySelector('.close_modal_btn');
       closeModal.addEventListener('click', () => {
         modalContainer.classList.add('desativo');
